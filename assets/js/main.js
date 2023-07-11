8cd753be8b176f0ba8bff7c418de47f2545e90ff
Dev Khandelwal <122960451+khandelwaldev@users.noreply.github.com>
2023-07-11 21:57:41 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: DevFolio
-* Updated: May 30 2023 with Bootstrap v5.3.0
-* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
